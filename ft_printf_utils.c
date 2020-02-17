0e0608d5a584310f99dfc3021e34c640704ec2d6
Jacquet BENJAMIN <jabenjam@e2r2p6.42.fr>
2020-02-17 14:53:12 +0100

    minor fix

--- ft_printf_utils.c
+++ ft_printf_utils.c
@@ -1,202 +1,133 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf_utils2.c                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jabenjam <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/11/11 14:47:18 by jabenjam          #+#    #+#             */
+/*   Updated: 2019/11/18 19:43:48 by jabenjam         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
 #include "ft_printf.h"
 
-int		ft_empty(s_struct *f)
+int		ft_isspace(int c)
 {
-	int		i;
-
-	i = 0;
-	if (f->width > 0)
-	{
-		while (i < f->width)
-		{
-//			write(1, &f->pad, 1);
-			i++;
-		}
-	}
-	return (i);
-}
-
-int		ft_print2(char *s, s_struct *f, int len, int mode)
-{
-	int		i;
-
-	i = 0;
-
-	if (mode == 1 && f->precision < len && f->precision)
-		len = f->precision;
-	if (*s && mode == 1 && f->precision > 0 && f->width == 0)
-	{
-		write(1, s, len);
-		i += len;
-	}
-	else if (*s && f->precision != -1)
-	{
-		write(1, s, len);
-		i += len;
-	}
-	else if (f->precision == -1)
-		f->width = len;
-	while (f->width > 0)
-	{
-//		i += ft_char(f->pad);
-		f->width--;
-	}
-	return (i);
-}
-
-int		ft_print(char *s, s_struct *f, int mode)
-{
-	int		i;
-	int		len;
-
-	if (s == NULL && mode == 1)
-		s = "(null)";
-	else if (!s)
-		return (ft_empty(f));
-	len = ft_strlen(s);
-	i = 0;
-	ft_adjust(f, mode, len);
-	if (mode == 1 && (f->null == 1 || (f->handle == 1 && f->width == 0 &&
-			f->precision == 0)))
+	if ((c >= 9 && c <= 13) || c == 32)
+		return (1);
+	else
 		return (0);
-	if (f->width > 0)
-	{
-		if (len >= f->width && mode == 1 && f->handle == 1)
-		{
-			len = f->width; //le bug se produit ici !!!
-			f->width = 0;
-		}
-		else
-			f->width = (f->precision > 0 && mode == 1 ? len : f->width - len);
-	}
-//	f->pad = (f->zero == 1 ? '0' : ' ');
-	while (f->left != 1 && f->width > 0)
-	{
-//		i += ft_char(f->pad);
-		f->width--;
-	}
-	return (i + (ft_print2(s, f, len, mode)));
 }
 
-int		ft_integer2(unsigned int n, char *s, s_struct *f)
+int		ft_isdigit(int c)
+{
+	if (c >= '0' && c <= '9')
+		return (1);
+	else
+		return (0);
+}
+
+int		ft_abs(int n)
+{
+	return (n < 0 ? (int64_t)-n : (int64_t)n);
+}
+
+int			ft_strlen(char *str)
+{
+	int			len;
+
+	len = 0;
+	while (str[len])
+		len++;
+	return (len);
+}
+
+char		*ft_toupper(char *str)
 {
 	int		i;
 
 	i = 0;
-	ft_adjust(f, 0, ft_ilen(n, 10));
-	while (n)
+	while (str[i])
 	{
-		*--s = (n % 10) + '0';
-		n /= 10;
+		if (str[i] >= 'a' && str[i] <= 'z')
+			str[i] -= '32';
+		i++;
 	}
-	while (f->precision > ft_strlen(s) && ft_strlen(s) < f->width)
-		*--s = '0';
-	if (f->neg)
-	{
-		if (f->width && f->zero)
-		{
-			i += ft_putchar('-');
-			f->width--;
-		}
-		else
-			*--s = '-';
-	}
-	return (i + (ft_print(s, f, 0)));
+	return (str);
 }
 
-int		ft_integer(int n, s_struct *f, int sign)
+int			ft_ilen(unsigned int n, int base)
 {
-	char	buffer[4096];
-	char	*s;
-	unsigned int nb;
+	int64_t		len;
+	int64_t		nb;
 
-	nb = n;
+	len = 0;
+	if (n < 0)
+		len++;
+	nb = ft_abs(n);
+	while (nb > base)
+	{
+		nb = nb / base;
+		len++;
+	}
+	return (len + 1);
+}
+
+void	*ft_memset(void *b, int c, size_t len)
+{
+	unsigned int	i;
+	unsigned char	uc;
+	unsigned char	*dst;
+
+	i = 0;
+	uc = c;
+	dst = b;
+	while (len--)
+		dst[i++] = uc;
+	return (b);
+}
+
+void	ft_bzero(void *s, size_t n)
+{
 	if (n == 0)
-	{
-		buffer[0] = '0';
-		buffer[1] = '\0';
-		return (ft_print(s, f, 0));
-	}
-	else if (sign == 1 && n < 0)
-	{
-		f->neg = 1;
-		nb = -n;
-	}
-	s = buffer + 4095;
-	*s = '\0';
-	return (ft_integer2(nb, s, f));
+		return ;
+	else
+		ft_memset(s, 0, n);
 }
 
-int		ft_hex2(uint64_t n, char *s, s_struct *f, int caps)
+char		*ft_strrev(char *str)
 {
-	int		i;
-	char	*hex;
-	char	*hex2;
+	int			i;
+	int			len;
+	char		tmp;
 
-	i = 0;
-	hex = "0123456789abcdef";
-	hex2 = "0123456789ABCDEF";
-	ft_adjust(f, 0, ft_ilen(n, 16));
-	while (n)
+	len = 0;
+	while (str[len])
+		len += 1;
+	i = -1;
+	while (++i < --len)
 	{
-		*--s = (caps == 1 ? hex2[(n % 16)]: hex[(n % 16)]);
-		n /= 16;
+		tmp = str[i];
+		str[i] = str[len];
+		str[len] = tmp;
 	}
-	if (f->pointer == 2)
-	{
-		*--s = 'x';
-		*--s = '0';
-	}
-	while (f->precision > ft_strlen(s) && ft_strlen(s) < f->width)
-		*--s = '0';
-	return (i + (ft_print(s, f, 0)) - f->pointer);
+	str[i] = '\0';
+	return (str);
 }
 
-int		ft_hex(uint64_t n, s_struct *f, int caps)
+int		ft_atoi(const char *str)
 {
-	char		buffer[4096];
-	char		*s;
-	uint64_t	nb;
+	size_t				result;
+	int					sign;
 
-	nb = n;
-	if (!n)
-	{
-		if (f->pointer == 2)
-		{
-			buffer[0] = '0';
-			buffer[1] = 'x';
-			buffer[2] = '0';
-			buffer[3] = '\0';
-			return (ft_print(buffer, f, 0) - 2);
-		}
-		else
-		{
-			buffer[0] = '0';
-			buffer[1] = '\0';
-		}
-		return (ft_print(buffer, f, 0));
-	}
-	s = buffer + 4095;
-	*s = '\0';
-	return (ft_hex2(nb, s, f, caps));
-}
-
-int		ft_char(int c, s_struct *f)
-{
-	if (f->left)
-		ft_putchar(c);
-	f->count = ft_width(f);
-	if (!f->left)
-		ft_putchar(c);
-}
-
-int			ft_pointer(void *adr, s_struct *f)
-{
-	int		len;
-	int		i;
-
-	len = ft_ilen((uint64_t)adr, 16);
-	f->pointer = 2;
-	return (ft_hex((uint64_t)adr, f, 0) + 2);
+	result = 0;
+	sign = 1;
+	while (ft_isspace(*str))
+		str++;
+	if (*str == '-' || *str == '+')
+		sign = *str == '-' ? -1 : 1;
+	while (*str >= '0' && *str <= '9')
+		result = (result * 10) + (*str++ - 48);
+	return (result * sign);
 }
